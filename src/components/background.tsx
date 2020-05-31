d512d03d62cc06ba33a8c0bb0db71b0baf6a60f6
Manuel Pacheco <manuelalejandropm@gmail.com>
2020-05-31 11:18:25 -0400

    fix minor issues in background component

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -18,19 +18,15 @@ const buildElems = (n: number) =>
   Array(n)
     .fill(0)
     .map(() => {
-      const option = random([1, 2, 4, 8, 7, 11, 13, 14])
+      // This defines in binary format which of the 4 corners are rounded
+      const figure = random([1, 2, 4, 8, 7, 11, 13, 14])
 
       const borderRadius = Array(4)
         .fill(0)
-        .map((_, j) => ((option >> j) & 1 ? "100%" : "0"))
+        .map((_, j) => ((figure >> j) & 1 ? "100%" : "0"))
         .join(" ")
 
-      const dir = `${Math.floor(Math.random() * 8) * 45}deg`
-      // const colors = ["#121212", "#f44336", "#ff7d12"]
-      const colors = ["#121212", "#f44336"]
-
-      const background = random(colors)
-      // const background = `linear-gradient(${dir}, ${start} 0%, ${end} 100%)`
+      const background = random(["#121212", "#f44336"])
       const opacity = random(["10%", "30%", "60%"])
 
       return { style: { borderRadius, background, opacity } }
@@ -52,8 +48,8 @@ const PureBackground = React.memo((props: Props) => {
 
   return (
     <div className={classes.root}>
-      {elems.map(elem => (
-        <div style={elem.style}></div>
+      {elems.map((elem, i) => (
+        <div key={i} style={elem.style}></div>
       ))}
     </div>
   )
