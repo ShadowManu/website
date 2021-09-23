75984636e115a1671ece595123a85e3bf24935df
Manuel Pacheco <manuelalejandropm@gmail.com>
2021-09-23 15:37:33 -0300

    simplify background

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,63 +1,31 @@
-import React, { useMemo } from "react";
+import styled, { keyframes } from "styled-components";
 
-import { useWindowSize } from "../core/use-window-size";
-import styled from "styled-components";
-
-interface Props {
-  rows: number;
-  columns: number;
-}
-
-const random = <T extends {}>(opts: T[]): T => {
-  const index = Math.floor(Math.random() * opts.length);
-  return opts[index];
-};
-
-const buildElems = (n: number) =>
-  Array(n)
-    .fill(0)
-    .map((_, key) => {
-      // This defines in binary format which of the 4 corners are rounded
-      const figure = random([1, 2, 4, 8, 7, 11, 13, 14]);
-
-      const borderRadius = Array(4)
-        .fill(0)
-        .map((_, j) => ((figure >> j) & 1 ? "100%" : "0"))
-        .join(" ");
-
-      const background = random(["#121212", "#f44336"]);
-      const opacity = random(["10%", "30%", "60%"]);
-
-      return { style: { borderRadius, background, opacity }, key };
-    });
-
-const Root = styled.div<Props>`
-  position: absolute;
-  z-index: -1;
-  width: 100%;
-  height: 100%;
-
-  background: ${({ theme }) => theme.palette.background.default};
-
-  display: grid;
-  grid-template-columns: ${({ columns }) => `repeat(${columns}, 1fr)`};
-  grid-template-rows: ${({ rows }) => `repeat(${rows}, 1fr)`};
+const gradient = keyframes`
+  0% { background-position: 0% 50%; }
+	50% { background-position: 100% 50%; }
+	100% { background-position: 0% 50%; }
 `;
 
-const Background = () => {
-  const size = useWindowSize();
+const rotate = keyframes`
+  0% { filter: hue-rotate(0deg); }
+  25% { filter: hue-rotate(-30deg); }
+  50% { filter: hue-rotate(0deg); }
+  75% { filter: hue-rotate(30deg); }
+	100% { filter: hue-rotate(0deg); }
+`;
 
-  const rows = Math.ceil((size?.height ?? 0) / 25);
-  const columns = Math.ceil((size?.width ?? 0) / 25);
-  const elems = useMemo(() => buildElems(rows * columns), [rows, columns]);
+const Background = styled.div`
+  position: absolute;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
 
-  return (
-    <Root rows={rows} columns={columns}>
-      {elems.map((elem, key) => (
-        <div key={key} style={elem.style} />
-      ))}
-    </Root>
-  );
-};
+  // Inspired by Manuel Pinto
+  // See more at https://codepen.io/P1N2O/pen/pyBNzX
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+
+  animation: ${gradient} 15s linear infinite, ${rotate} 10s linear infinite;
+`;
 
 export default Background;
