9cc585ae7b72c64f2605a9dd62f522eed29bafe8
Manuel Pacheco <manuelalejandropm@gmail.com>
2021-09-23 13:16:16 -0300

    adjust config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,30 +6,29 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+      options: { name: `images`, path: `${__dirname}/src/images` },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#121212`,
-        theme_color: `#f44336`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-styled-components`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#121212`,
+    //     theme_color: `#f44336`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `gatsby-plugin-material-ui`,
   ],
-}
+};
