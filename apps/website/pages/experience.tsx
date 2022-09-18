3a48443da8c0201267086aba310a9541291d62cc
Manuel Pacheco <contact@manuelpacheco.dev>
2022-09-18 09:31:54 -0300

    add skeleton experience page

---
+++ apps/website/pages/experience.tsx
@@ -0,0 +1,16 @@
+import Typography from '@mui/material/Typography';
+import SEO from '../components/seo';
+
+import Shell from '../components/shell';
+
+const Index = () => {
+  return (
+    <Shell centered>
+      <SEO />
+
+      <Typography variant="body1">TODO</Typography>
+    </Shell>
+  );
+};
+
+export default Index;
